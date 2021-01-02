228635638f015e4d36af7f40f9d5af0fe59befa8
SNU\paryoja <paryoja@hanmail.net>
2021-01-02 21:08:53 +0900

    add new files

--- django_server/app_server/settings.py
+++ django_server/app_server/settings.py
@@ -24,7 +24,7 @@ env = environ.Env()
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
